b5278aa485c12edd8773908e291b02c434502203
Grady Fields <ramjacvp@gmail.com>
2016-12-22 07:22:56 -0500

    Version 1 complete. Verbose, retries, time, and csv flags work.
    Need to improve testing and verbose output then implement JSON file input.

--- main.go
+++ main.go
@@ -24,78 +24,33 @@ type HTTPTest struct {
 	expectMatch    bool
 }
 
+var (
+	verbose bool
+	info    *maybeLogger
+)
+
 func main() {
 	// read flags
-	retries := flag.Int("r", 5, "Number of retries for HTTP requests (defaults to 5)")
-	timeElapse := flag.Int("t", 5, "Time elapse multiplier used between HTTP request retries in seconds (defaults to 5)")
+	retries := flag.Int("r", 5, "Number of retries for HTTP requests (defaults to 5).")
+	timeElapse := flag.Int("t", 5, "Time elapse multiplier used between HTTP request retries in seconds (defaults to 5).")
 	rawCsv := flag.String("csv", "", "<url>,<headers as key1=value1&key2=value2>,<expected HTTP status code>,<expected content type>,<regex>,<bool regex should return data>")
+	jsonFile := flag.String("json", "", "Path and name of the json request file.")
 	//concurrency := flag.Int("c", 0, "Number of requests to make concurrently (defaults to 1)")
+	flag.BoolVar(&verbose, "v", false, "Prints resutls of each step. Also causes all tests to execute instead of returning after the first failure.")
 
 	flag.Parse()
 
-	// read csv
-
-	// parse csv to structs
 	var r []*HTTPTest
-	tmpClient := new(HTTPTest)
+	info = new(maybeLogger)
 
-	colCount := 0
-
-	for _, v := range strings.Split(*rawCsv, ",") {
-		v = strings.TrimSpace(v)
-
-		switch colCount {
-		case 0:
-			tmpClient.request = new(http.Request)
-			u, err := url.Parse(v)
-			if err == nil {
-				tmpClient.request.URL = u
-			} else {
-				log.Println(err)
-			}
-		case 1:
-			setHeaders(tmpClient, v)
-		case 2:
-			s, err := strconv.Atoi(v)
-			if err == nil {
-				tmpClient.expectedStatus = s
-			} else {
-				log.Printf("Error parsing status code: %s\n", err)
-			}
-		case 3:
-			tmpClient.expectedType = v
-		case 4:
-			if len(v) > 0 {
-				s, err := regexp.Compile(v)
-				if err != nil {
-					log.Printf("Error parsing regular expression: %s\n", err)
-				} else {
-					tmpClient.regex = s
-				}
-			}
-		case 5:
-			if len(v) > 0 {
-				s, err := strconv.ParseBool(v)
-				if err != nil {
-					log.Printf("Error parsing the boolean for whether the regex should match or not: %s\n", err)
-				} else {
-					tmpClient.expectMatch = s
-				}
-			}
-			// add the tmpClient to the slice if it is valid
-			// if tmpClient has a url and expected status code set then append to r
-
-			if tmpClient.request.URL != nil &&
-				tmpClient.expectedStatus > 0 {
-				r = append(r, tmpClient)
-			}
-
-			tmpClient = new(HTTPTest)
-
-			colCount = 0
-			continue
-		}
-		colCount++
+	switch {
+	case len(*jsonFile) > 0:
+		log.Fatal("JSON file support not yet implemented")
+	case len(*rawCsv) > 0:
+		// parse csv to structs
+		r = parseCSV(rawCsv)
+	default:
+		log.Fatal("A JSON or CSV input is required")
 	}
 
 	// make HTTP requests
@@ -111,10 +66,77 @@ Tests:
 			}
 		}
 		failures++
+		if !verbose {
+			break
+		}
 	}
 
 	// return success/failure
 	fmt.Println(failures)
+}
+
+func parseCSV(rawCSV *string) (r []*HTTPTest) {
+	tmpClient := new(HTTPTest)
+
+	colCount := 0
+
+	for _, v := range strings.Split(*rawCSV, ",") {
+		v = strings.TrimSpace(v)
+
+		switch colCount {
+		case 0:
+			tmpClient.request = new(http.Request)
+			u, err := url.Parse(v)
+			if err == nil {
+				tmpClient.request.URL = u
+			} else {
+				info.Println(err)
+			}
+		case 1:
+			setHeaders(tmpClient, v)
+		case 2:
+			s, err := strconv.Atoi(v)
+			if err == nil {
+				tmpClient.expectedStatus = s
+			} else {
+				info.Printf("Error parsing status code: %s\n", err)
+			}
+		case 3:
+			tmpClient.expectedType = v
+		case 4:
+			if len(v) > 0 {
+				s, err := regexp.Compile(v)
+				if err != nil {
+					info.Printf("Error parsing regular expression: %s\n", err)
+				} else {
+					tmpClient.regex = s
+				}
+			}
+		case 5:
+			if len(v) > 0 {
+				s, err := strconv.ParseBool(v)
+				if err != nil {
+					info.Printf("Error parsing the boolean for whether the regex should match or not: %s\n", err)
+				} else {
+					tmpClient.expectMatch = s
+				}
+			}
+			// add the tmpClient to the slice if it is valid
+			// if tmpClient is valid when it has a url and expected status code
+			if tmpClient.request.URL != nil &&
+				tmpClient.expectedStatus > 0 {
+				r = append(r, tmpClient)
+			}
+
+			tmpClient = new(HTTPTest)
+
+			colCount = 0
+			continue
+		}
+		colCount++
+	}
+
+	return r
 }
 
 func checkRequest(ht *HTTPTest) bool {
@@ -124,10 +146,13 @@ func checkRequest(ht *HTTPTest) bool {
 	client := &http.Client{}
 	//fmt.Println(ht.request)
 	resp, err := client.Do(ht.request)
-	log.Printf("Response: %v", resp)
+	info.Printf("Response: %v", *resp)
 	if err == nil &&
-		resp.StatusCode == ht.expectedStatus &&
-		(strings.Compare(resp.Header.Get("content-type"), ht.expectedType) == 0 || len(ht.expectedType) == 0) {
+		resp.StatusCode == ht.expectedStatus {
+		if len(ht.expectedType) > 0 &&
+			strings.Compare(resp.Header.Get("content-type"), ht.expectedType) != 0 {
+			return false
+		}
 
 		if ht.regex != nil {
 			buf := new(bytes.Buffer)
@@ -143,9 +168,9 @@ func checkRequest(ht *HTTPTest) bool {
 	}
 
 	if err != nil {
-		log.Printf("Error on Get: %s\n", err)
+		info.Printf("Error on Get: %s\n", err)
 	} else {
-		log.Printf("Error in response: %v\n", resp)
+		info.Printf("Error in response: %v\n", resp)
 	}
 	return false
 }
@@ -170,7 +195,22 @@ func setHeaders(ht *HTTPTest, h string) {
 		if err == nil {
 			ht.request.Header.Set(k, v)
 		} else {
-			log.Println(err)
+			info.Println(err)
 		}
+	}
+}
+
+// maybeLogger only logs when the verbose variable is true. I don't like the name, but I'm tired.
+type maybeLogger struct{}
+
+func (l *maybeLogger) Println(v ...interface{}) {
+	if verbose {
+		log.Println(v)
+	}
+}
+
+func (l *maybeLogger) Printf(s string, v ...interface{}) {
+	if verbose {
+		log.Printf(s, v)
 	}
 }
