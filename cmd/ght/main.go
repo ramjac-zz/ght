c0607cd6b867d619be80ab41bff8d41bc166c9d4
Grady Fields <ramjacvp@gmail.com>
2017-07-29 00:10:39 -0400

    Tweaking the failures count  logging.

--- cmd/ght/main.go
+++ cmd/ght/main.go
@@ -88,6 +88,7 @@ func main() {
 	wg.Wait()
 
 	// return success/failure
-	logger.Println("Failing tests:", failTests)
+	logger.Println("Failing tests: ", failTests)
+	logger.Println("Failures: ", failures)
 	os.Exit(failures)
 }
